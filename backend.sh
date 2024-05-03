233b9b03f52983e9a48696859bc795f88ee7ce39
Leela Kamineni <kaminenileela1992@gmail.com>
2024-05-03 01:22:36 -0400

    error handling expense project

--- backend.sh
+++ backend.sh
@@ -9,54 +9,62 @@ read -s PASSWORD
 
 
 dnf module disable nodejs -y &>>$LOGFILE
-VALIDATE $? "Disabling default nodejs"
+#VALIDATE $? "Disabling default nodejs"
 
 dnf module enable nodejs:20 -y &>>$LOGFILE
-VALIDATE $? "Enabling nodejs version 20"
+#VALIDATE $? "Enabling nodejs version 20"
 
 dnf install nodejs -y &>>$LOGFILE
-VALIDATE $? "Installing nodejs"
+#VALIDATE $? "Installing nodejs"
 
-id expense &>>$LOGFILE
+useradd(){
 
-if [ $? -ne 0 ]
-then
-    useradd expense &>>$LOGFILE
-    VALIDATE $? "creating expense user"
-else
-     echo -e "Expense user already created ... $Y SKIPPING $N"
-fi
+    useradd expense || true
+}
+
+useradd
+
+# id expense &>>$LOGFILE
+
+# if [ $? -ne 0 ]
+# then
+#     useradd expense &>>$LOGFILE
+#     VALIDATE $? "creating expense user"
+# else
+#      echo -e "Expense user already created ... $Y SKIPPING $N"
+# fi
 
 mkdir -p /app &>>$LOGFILE
-VALIDATE $? "creating app directory"
+#VALIDATE $? "creating app directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
-VALIDATE $? "downloading backend code"
+#VALIDATE $? "downloading backend code"
 
 cd /app
 rm -rf /app/*
 unzip /tmp/backend.zip &>>$LOGFILE
-VALIDATE $? "Extracted backend code"
+#VALIDATE $? "Extracted backend code"
 
 npm install &>>$LOGFILE
-VALIDATE $? "installing nodejs dependencies"
+#VALIDATE $? "installing nodejs dependencies"
 
 cp /home/ec2-user/expense-project-shell/backend.service /etc/systemd/system/backend.service
-VALIDATE $? "copied backend service"
+#VALIDATE $? "copied backend service"
 
 systemctl daemon-reload &>>$LOGFILE
-VALIDATE $? "Daemon Reload"
+#VALIDATE $? "Daemon Reload"
 systemctl start backend &>>$LOGFILE
 systemctl enable backend &>>$LOGFILE
-VALIDATE $? "Starting and Enabling backend"
+#VALIDATE $? "Starting and Enabling backend"
 
 dnf install mysql -y &>>$LOGFILE
-VALIDATE $? "Installing mysql client"
+#VALIDATE $? "Installing mysql client"
 
 
 mysql -h db.learningdevopsaws.online -uroot -p$PASSWORD < /app/schema/backend.sql &>>$LOGFILE
-VALIDATE $? "Schema loading"
+#VALIDATE $? "Schema loading"
 
 
 systemctl restart backend &>>$LOGFILE
-VALIDATE $? "restaring backend"
+#VALIDATE $? "restaring backend"
+echo "System restarted"
