facabdaaa1ae50e5e03fe1d575016450d2cc2ca4
Leela Kamineni <kaminenileela1992@gmail.com>
2024-05-03 01:36:30 -0400

    error handling expense project

--- frontend.sh
+++ frontend.sh
@@ -4,29 +4,30 @@ source ./common.sh
 check_root
 
 dnf install nginx -y &>>$LOGFILE
-VALIDATE $? "Installing ngnix"
+#VALIDATE $? "Installing ngnix"
 
 systemctl enable nginx &>>$LOGFILE
-VALIDATE $? "Enabling ngnix"
+#VALIDATE $? "Enabling ngnix"
 
 systemctl start nginx &>>$LOGFILE
-VALIDATE $? "Starting ngnix"
+#VALIDATE $? "Starting ngnix"
 
 rm -rf /usr/share/nginx/html/* &>>$LOGFILE
-VALIDATE $? "Removing existing content"
+#VALIDATE $? "Removing existing content"
 
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
-VALIDATE $? "Downloading frontend code"
+#VALIDATE $? "Downloading frontend code"
 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>$LOGFILE
-VALIDATE $? "Extracting frontend code"
+#VALIDATE $? "Extracting frontend code"
 
 cp /home/ec2-user/expense-project-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
-VALIDATE $? "copied expense.conf"
+#VALIDATE $? "copied expense.conf"
 
 systemctl restart nginx &>>$LOGFILE
-VALIDATE $? "restarting ngnix"
+#VALIDATE $? "restarting ngnix"
 
+echo "System restarted"
 
 
